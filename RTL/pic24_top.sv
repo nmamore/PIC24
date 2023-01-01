7d357e92085cda5d0cc2e17c3126c3ecff65bfb6
Nick A <namore7@gmail.com>
2022-12-31 20:06:18 -0500

    Program Counter
    Commit initial creation of program counter

--- RTL/pic24_top.sv
+++ RTL/pic24_top.sv
@@ -1,17 +1,36 @@
 /*
 * @file pic24_top.sv
 * @brief top-level file for the PIC24
+* Implements a Harvard Architecture with 16-bit data bus
 * @author Nicholas Amore namore7@gmail.com
 * @date Created 12/29/2022
 */
 
 `timescale 1ns / 100ps
 
-module hex_sequencing_computer_top
+module pic24_top
 (
-	input logic i_clk_50M,
-	input logic i_rstn
+	input logic clk50M_i,
+	input logic rst_ni
 	
+);
+
+logic [15:0] pic_databus;
+
+logic [23:0] pc_addr;
+
+//Control Signals
+logic pc_load;
+logic pc_inc;
+
+
+program_counter i_program_counter(
+	.clk_i	  (clk50M_i),
+	.rst_ni	  (rst_ni),
+	.pcload_i (pc_load),
+	.pcinc_i  (pc_inc),
+	.databus_i(pic_databus),
+	.pc_addr_o(pc_addr)	
 );
 
 endmodule
---
+++ RTL/program_counter.sv
@@ -0,0 +1,93 @@
+/*
+* @file program_counter.sv
+* @brief top-level file for the PIC24 program counter
+* 23 bit PC for PIC24. Only access memory on even words.
+* Can only access up to 7FFFFE. Allows for up to 2^22 
+* unique potential address spaces.
+* @author Nicholas Amore namore7@gmail.com
+* @date Created 12/30/2022
+*/
+
+`timescale 1ns / 100ps
+
+module program_counter
+(
+	input  logic		clk_i,
+	input  logic		rst_ni,
+	
+	input  logic		pcload_i,
+	input  logic		pcinc_i,
+	
+	input  logic [15:0]	databus_i,
+	output logic [23:0]	pc_addr_o	
+);
+
+//Internal count for PC
+//PC is only 23 bit, with LSb fixed to 0 so only 22 addresses are accessible
+logic [21:0] = pc_addr;
+
+//MSb is set to 0 as all PC accesible code is below 7FFFFE
+//LSb is set to 0 to ensure only even addresses are accessed
+assign pc_addr_o = {1'b0, pc_addr, 1'b0};
+
+//Define the states
+typdef enum{
+	StIdle, StReset, StInc, StLoad1, StLoad2
+} pc_state_e;
+
+pc_state_e pc_state_d, pc_state_q;
+
+//Combinational decode of the states
+always_comb begin
+	pc_state_d = pc_state_q;
+	case(pc_state_q)
+		//StIdle: Waits for control signal to increment or load address
+		StIdle: begin
+			//Checks if control has indicated an address load
+			if(pcload_i) begin
+				pc_state_d = StLoad1;
+			end
+			//Checks if control has indicated an address increment
+			else if (pcinc_i) begin
+				pc_state_d = StInc;
+			end
+		end
+		//StReset: Reset state of PC. Sets initial address back to 0
+		StReset: begin
+			pc_addr = 22'h000000;
+			pc_state_d = StIdle;
+		end
+		//StInc: Increments the PC
+		StInc: begin
+			pc_addr = 22'h000000 + 1'b1;
+			pc_state_d = StIdle;
+		end
+		//StLoad1: Loads in the LSW of the address from the databus
+		StLoad1: begin
+			//LSb of databus is always 0; Already added to end of PC by this file
+			pc_addr[14:0] = databus_i[15:1];
+			pc_state_d = StLoad2;
+		end
+		//StLoad2: Loads in the MSW of the address from the databus
+		StLoad2: begin
+			//MSb of databus is always 0; Already added to start of PC by this file
+			pc_addr [21:15] = databus_i[6:0];
+			pc_state_d = StIdle;
+		end
+		default: begin
+			pc_state_d = StIdle;
+		end
+	endcase
+end
+
+//Register the state
+always_ff @(posedge clk_i or negedge rst_ni) begin
+	if (!rst_ni) begin
+		pc_state_q <= StReset;
+	end
+	else begin
+		pc_state_q <= pc_state_d;
+	end
+end
+
+endmodule
